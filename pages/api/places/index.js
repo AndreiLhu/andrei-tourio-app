a0ea7d293c57fc9e3b417c1f6db31f3db533b4be
AndreiLhu <leahu.andreilucian@gmail.com>
2024-01-10 21:51:54 +0100

    DB connected. work in progress

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,23 @@
-import { db_places } from "../../../lib/db_places";
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
 
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+
+    return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+
+      return response.status(201).json({ status: "Place created." });
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
+    }
+  }
 }
